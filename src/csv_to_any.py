2333e2733e70c2bd51691bacc08e8db6132feb54
Swaraj Jalkote <swarajjalkote98@gmail.com>
2022-11-27 16:25:08 +0530

    Added boilerplate code for csv to any conversion

---
+++ src/csv_to_any.py
@@ -0,0 +1,26 @@
+
+
+class CSVTOANY:
+    def __init__(self, input_filename, input_delimiter, output_filename, output_delimiter):
+        self.input_filename = input_filename
+        self.input_delimiter = input_delimiter
+        self.output_filename = output_filename
+        self.output_delimiter = output_delimiter
+
+    def csv_to_dat(self):
+        pass
+
+    def csv_to_json(self):
+        pass
+    
+    def csv_to_excel(self):
+        pass
+
+    def csv_to_xml(self):
+        pass
+
+    def csv_to_parquet(self):
+        pass
+
+    def csv_to_avro(self):
+        pass
--- src/unicov.py
+++ src/unicov.py
@@ -1,15 +1,30 @@
 import pandas as pd
-
+from . import csv_to_any as csv_proc
 
 class Unicov():
     ''' This class will be used to create a object and then use to read the file data and get it's type'''
-    pass
+    def __init__(self, input_filename, input_delimiter, output_filename, output_delimiter="|"):
+        self.input_filename = input_filename
+        self.input_delimiter = input_delimiter
+        self.output_filename = output_filename
+        self.output_delimiter = output_delimiter
 
-    # def read_file(self, input_file, delimiter=""):
-    #     ''' This function will read the file and get the data as well as file type and any other required parameters'''
-    #     pass
+    def read_file(self):
+        ''' This function will read the file and get the data as well as file type and any other required parameters'''
+        response = ""
+        if self.input_filename.endswith(".csv"):
+            response = csv_proc.CSVTOANY(input_filename = self.input_filename, 
+                                                  input_delimiter = self.input_delimiter, 
+                                                  output_filename = self.output_filename, 
+                                                  output_delimiter = self.output_delimiter)
+        
+        
+        
+        
+        elif self.filename.endswith():
+            pass
 
-    #     # return file_data, file_type
+        return response
 
 
 
